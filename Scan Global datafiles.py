7c2778ec69e0ffc3b0da728c2f09fb2cbd319963
Nmobki <nmo@bki.dk>
2020-11-03 13:48:23 +0100

    Update Scan Global datafiles.py

--- Scan Global datafiles.py
+++ Scan Global datafiles.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import os.path
+import pandas as pd
+from sqlalchemy import create_engine
+import pyodbc
 
 Path_source = r'\\filsrv01\bki\11. Økonomi\04 - Controlling\NMO\1. Produktion\Råkaffe\Scan Global filer'
 Path_archive = r'\\filsrv01\bki\11. Økonomi\04 - Controlling\NMO\1. Produktion\Råkaffe\Scan Global filer\Arkiv'
@@ -10,14 +13,18 @@ File_complete = Path_source + File_name + '.xlsx'
 File_timestamp = os.path.getctime(File_complete)
 File_complete_new = Path_source + File_name + '_' + str(File_timestamp) + '.xlsx'
 
+server = 'sqlsrv04'
+db = 'BKI_Datastore'
+con = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + db)
+
 print( File_complete )
 print( File_complete_new )
 
-os.path.exists( File_complete)
+if os.path.exists( File_complete): #Check if file exists
+    os.rename( File_complete , File_complete_new ) #rename file from creating time
+    
+    df_sg = pd.read_excel(File_complete_new, header=0).to_dict()
+    
+print( df_sg )
 
 
-print(os.path.exists( File_complete))
-
-print(os.path.getctime(File_complete))
-
-# os.path.getmtime(path)
