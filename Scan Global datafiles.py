e98027c041336e8a6fc1119b2d21eff4e8d8a942
Nmobki <nmo@bki.dk>
2021-11-29 11:33:32 +0100

    Update Scan Global datafiles.py
    Create entry into email log if any error occurs during attempted read and insert of file

--- Scan Global datafiles.py
+++ Scan Global datafiles.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 import urllib
 import datetime
 import shutil
-import numpy as np
+
 
 Path_source = r'\\filsrv01\bki\11. Økonomi\04 - Controlling\NMO\1. Produktion\Råkaffe\Scan Global filer'
 Path_archive = r'\\filsrv01\bki\11. Økonomi\04 - Controlling\NMO\1. Produktion\Råkaffe\Scan Global filer\Arkiv'
@@ -34,19 +34,30 @@ Cols_df_sg = ['Kontraktnummer' ,'Containernummer' ,'Bill_of_lading' ,'Nettovægt
 
 Df_log = pd.DataFrame(data= {'Date':Timestamp ,'Event':Script_name ,'Note': 'Filename: ' + File_name_new }, index=[0])
 
-if os.path.exists( File_complete): # Check if file exists
-    os.rename( File_complete , File_complete_new ) # Rename file from creation time
+try:
+    if os.path.exists( File_complete): # Check if file exists
+        os.rename( File_complete , File_complete_new ) # Rename file from creation time
+        
+        Dic_file = pd.read_excel(File_complete_new, header=0).to_dict() # Read file into dictionary
+        Df_sg = pd.DataFrame.from_dict(Dic_file) # Read dictionary into dataframe
+        Df_sg.columns = Df_sg.columns.str.replace('\n', '') # Replace newline characters from column headers
+        Df_sg = Df_sg.rename(columns=Cols_df_sg_rename) #Rename columns
+        Df_sg = Df_sg[Cols_df_sg] # Limit columns to those present in SQL target table
+        Df_sg = Df_sg.replace({'nan': None , 'NONE': None}) # Convert NaN values to 0 before SQL insert
+        Df_sg.loc[:, 'Timestamp'] = Timestamp
+        Df_sg.loc[:, 'Filnavn'] = File_name_new
+     
+        Df_sg.to_sql('Container_data' ,con=Engine ,schema=Schema ,if_exists='append' ,index=False) # Insert into SQL
+        Df_log.to_sql('Log' ,con=Engine ,schema='dbo' ,if_exists='append' ,index=False) # Write to log
     
-    Dic_file = pd.read_excel(File_complete_new, header=0).to_dict() # Read file into dictionary
-    Df_sg = pd.DataFrame.from_dict(Dic_file) # Read dictionary into dataframe
-    Df_sg.columns = Df_sg.columns.str.replace('\n', '') # Replace newline characters from column headers
-    Df_sg = Df_sg.rename(columns=Cols_df_sg_rename) #Rename columns
-    Df_sg = Df_sg[Cols_df_sg] # Limit columns to those present in SQL target table
-    Df_sg = Df_sg.replace({'nan': None , 'NONE': None}) # Convert NaN values to 0 before SQL insert
-    Df_sg.loc[:, 'Timestamp'] = Timestamp
-    Df_sg.loc[:, 'Filnavn'] = File_name_new
- 
-    Df_sg.to_sql('Container_data' ,con=Engine ,schema=Schema ,if_exists='append' ,index=False) # Insert into SQL
-    Df_log.to_sql('Log' ,con=Engine ,schema='dbo' ,if_exists='append' ,index=False) # Write to log
-
-    shutil.move(File_complete_new ,Path_archive + File_name_new)
+        shutil.move(File_complete_new ,Path_archive + File_name_new)
+except Exception as e:
+    df_email_err = pd.DataFrame(data= {
+        'Email_til': 'nmo@bki.dk' ,'Email_emne': 'Fejl i indlæsning af Scan Global fil'
+         ,'Email_tekst':'Indlæsning af ' + str(File_complete) + ' fra Scan Global er fejlet. \n\n'
+         + 'Nyt filnavn: ' + str(File_complete_new) + '\n\n'
+         + 'Følgende fejl er opstået: \n'
+         + str(e)
+    } ,index=[0])
+    # Create record in Email log if script fails and a files existed
+    df_email_err.to_sql('Email_log' ,con=Engine ,schema= 'dbo' ,if_exists='append' ,index=False)
